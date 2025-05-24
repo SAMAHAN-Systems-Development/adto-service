3fdf3c6051f43a24398b6369a7275f18668e3ec4
derickeroy2123 <derickeroy2123@gmail.com>
2025-05-24 16:01:22 +0800

    chore: change port and cors config

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(8000);
+  await app.listen(8080);
 }
 bootstrap();
