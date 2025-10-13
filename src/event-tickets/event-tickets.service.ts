93edf421fcc29a1b014fb83f0cf698f44809e919
Aycees <adolfocedric@gmail.com>
2025-10-14 01:14:26 +0800

    Imported prisma service to event tickets module

--- src/event-tickets/event-tickets.service.ts
+++ src/event-tickets/event-tickets.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEventTicketDto } from './dto/create-event-ticket.dto';
 import { UpdateEventTicketDto } from './dto/update-event-ticket.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class EventTicketsService {
+  constructor(
+    private readonly prisma: PrismaService,
+  ) {}
+
   create(createEventTicketDto: CreateEventTicketDto) {
     return 'This action adds a new eventTicket';
   }
